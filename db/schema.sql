956b2f03fe0a70c304cdfb84e3d31abee2eb1c0c
Optical23 <aaroneferguson@gmail.com>
2022-01-26 19:56:44 -0600

    added tags collumns

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS vriend_db;
-
+CREATE DATABASE vriend_db;
 USE vriend_db;
 
 CREATE TABLE game(
@@ -12,6 +12,12 @@ CREATE TABLE player(
     player_name VARCHAR(30) NOT NULL,
     player_email VARCHAR(30) NOT NULL,
     player_password VARCHAR(30) NOT NULL,
+    tag_casual BOOLEAN,
+    tag_forfun BOOLEAN,
+    tag_ranked BOOLEAN,
+    tag_experienced BOOLEAN,
+    tag_new BOOLEAN,
+    tag_microphone BOOLEAN,
     player_bio VARCHAR(120),
     player_friend INTEGER,
     FOREIGN KEY (player_friend)
