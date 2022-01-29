ed295b9d936d91e6be9370bab42969295e0c1f4d
Chau Pham <ncp9988@gmail.com>
2022-01-29 21:46:48 +0700

    start adding home-routes

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,1 +1,13 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Game, Player, Tag } = require('../models');
+
+// Get all ..
+
+// Login
+
+//
+
+
+module.exports = router;
+
