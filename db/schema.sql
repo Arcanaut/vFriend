aa09a2d7b86b2854a645ed0e1328de966379ca75
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-02-01 16:45:01 -0600

    refactored folder path for clarity

--- db/schema.sql
+++ db/schema.sql
@@ -2,19 +2,19 @@ DROP DATABASE IF EXISTS vriend_db;
 
 USE vriend_db;
 
-CREATE TABLE game(
-    game_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY,
-    game_title VARCHAR(30) NOT NULL
-)
+-- CREATE TABLE game(
+--     game_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY,
+--     game_title VARCHAR(30) NOT NULL
+-- )
 
-CREATE TABLE player(
-    player_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY,
-    player_name VARCHAR(30) NOT NULL,
-    player_email VARCHAR(30) NOT NULL,
-    player_password VARCHAR(30) NOT NULL,
-    player_bio VARCHAR(120),
-    player_friend INTEGER,
-    FOREIGN KEY (player_friend)
-    REFERENCES player(player_id)
-    ON DELETE SET NULL,
-)
+-- CREATE TABLE player(
+--     player_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY,
+--     player_name VARCHAR(30) NOT NULL,
+--     player_email VARCHAR(30) NOT NULL,
+--     player_password VARCHAR(30) NOT NULL,
+--     player_bio VARCHAR(120),
+--     player_friend INTEGER,
+--     FOREIGN KEY (player_friend)
+--     REFERENCES player(player_id)
+--     ON DELETE SET NULL,
+-- )
